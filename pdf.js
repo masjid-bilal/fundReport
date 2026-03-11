44e2da5dc33e68c32fded9d9c682aa559e8d9240
masjid-bilal <masjidbilal2014@gmail.com>
2026-03-11 23:08:29 +0530

    Update pdf.js

--- pdf.js
+++ pdf.js
@@ -2,6 +2,6 @@ function downloadPDF(){
 
 const element = document.getElementById("tableArea");
 
-html2pdf().from(element).save("table-data.pdf");
+html2pdf().from(element).save("Masjid_Bilal_Report.pdf");
 
 }
