29afe3300b06357e74f92d7e3e1f172ff8f3c3e8
masjid-bilal <masjidbilal2014@gmail.com>
2026-03-13 17:46:42 +0530

    Update and rename stamp.js to receipt.js

---
+++ receipt.js
@@ -0,0 +1,20 @@
+function downloadReceipt(btn){
+
+let row = btn.parentNode.parentNode;
+
+let name = row.cells[0].innerText;
+let desc = row.cells[1].innerText;
+let amount = row.cells[2].innerText;
+let mode = row.cells[3].innerText;
+
+document.getElementById("r_name").innerText = name;
+document.getElementById("r_desc").innerText = desc;
+document.getElementById("r_amount").innerText = "₹"+amount;
+document.getElementById("r_mode").innerText = mode;
+document.getElementById("r_total").innerText = amount;
+
+let element = document.getElementById("receipt");
+
+html2pdf().from(element).save("receipt.pdf");
+
+}
