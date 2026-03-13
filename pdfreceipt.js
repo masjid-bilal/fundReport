4712479ddc3a8c2bc65a2c6d5e964f93f3758e83
masjid-bilal <masjidbilal2014@gmail.com>
2026-03-13 21:03:30 +0530

    Update pdfreceipt.js

--- pdfreceipt.js
+++ pdfreceipt.js
@@ -2,6 +2,14 @@ function downloadPDF(){
 
 const element = document.querySelector(".receipt");
 
-html2pdf().from(element).save("cash_receipt.pdf");
+const opt = {
+  margin:10,
+  filename:'donation_receipt.pdf',
+  image:{ type:'jpeg', quality:1 },
+  html2canvas:{ scale:3 },
+  jsPDF:{ unit:'mm', format:'a4', orientation:'portrait' }
+};
+
+html2pdf().set(opt).from(element).save();
 
 }
